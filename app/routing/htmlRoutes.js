31a1c3f82c1373df8cab1653edd2411285032650
Brian Russell <brianrobertrussell@gmail.com>
2017-10-19 14:50:23 -0600

    files and server set up

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function(app){
+	//A default, catch-all route that leads to `home.html` which displays the home page. 
+	app.get("/", function(req, res) {
+   		res.sendFile(path.join(__dirname, "../public/home.html"))
+   	});
+	//A GET Route to `/survey` which should display the survey page.
+	app.get("/survey", function(req, res) {
+   		res.sendFile(path.join(__dirname, "../public/survey.html"))
+   	});
+};//end module
---
+++ server.js
@@ -0,0 +1,22 @@
+/////dependancies/////
+const express = require ("express");
+const bodyParser = require ("body-parser");
+const path = require ("path");
+
+/////appress app estup/////
+var app = express();
+var PORT =3000;
+
+/////data parsing setup/////
+//??????
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/////Router file locations
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+/////listener/////
+app.listen(PORT, function() {
+  console.log("appress App listening on PORT: " + PORT);
+});
